d6cb821fe133a59d977a92c945a6f5ab5a8173d4
ibrahiimnawzad <ibrahiimnawzad@gmail.com>
2025-07-02 20:16:32 +0300

    hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import './Hero.css';
+import HeroImage from '../assets/hero-img.png';
+import HeroImageExp from '../assets/hero-img-exp.png';
 
 const Hero = () => {
   return (
     <section className="hero-section">
-      <div className="hero-glow"></div>
-
-      <div className="hero-left">
-        <h1 className="hero-heading">
-          <span className="hero-letter-large">I</span>
-          <span className="hero-letter-small">BRAHIIM</span>
-        </h1>
-        <h2 className="hero-subheading">
-          <span className="hero-letter-large">N</span>
-          <span className="hero-letter-small">AWZAD</span>
-        </h2>
-        <p className="hero-tagline">Creative Developer</p>
-      </div>
-
-      <div className="hero-right">
-        {/* 3D Shape */}
-      </div>
+      {/* Background Image */}
+      <img className="hero-image-background" src={HeroImageExp} alt="Background Hero" />
+      
+      {/* Foreground Image */}
+      <img className="hero-image-foreground" src={HeroImage} alt="Hero" />
     </section>
   );
 };
